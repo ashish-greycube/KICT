c414f6c3dbc5f768bc1b9922d9ef584e1bfdb798
durve-greycube <zelam@greycube.in>
2024-05-10 14:34:42 +0530

    fix:calculation error

--- kict/kict/doctype/hourly_operations/hourly_operations.py
+++ kict/kict/doctype/hourly_operations/hourly_operations.py
@@ -14,15 +14,17 @@ class HourlyOperations(Document):
 	def set_day_tonnage_and_running_total(self):
 		ho_details = self.get("hourly_operation_details")
 		for row in ho_details:
-			if ho_details.index(row) == 0:
-				row.period_tonnage_running_total = row.hourly_receipt_qty
-				row.day_tonnage = self.total_tonnage_mt - row.period_tonnage_running_total
-			else :
-				row.period_tonnage_running_total = row.hourly_receipt_qty + ho_details[ho_details.index(row)-1].period_tonnage_running_total
-				row.day_tonnage = ho_details[ho_details.index(row)-1].day_tonnage - row.hourly_receipt_qty
+			if row.hourly_receipt_qty:
+				if ho_details.index(row) == 0:
+					row.period_tonnage_running_total = row.hourly_receipt_qty
+					row.day_tonnage = self.total_tonnage_mt - row.period_tonnage_running_total
+				else :
+					row.period_tonnage_running_total = row.hourly_receipt_qty + ho_details[ho_details.index(row)-1].period_tonnage_running_total
+					row.day_tonnage = ho_details[ho_details.index(row)-1].day_tonnage - row.hourly_receipt_qty
 
 	def set_balance(self):
 		ho_details = self.get("hourly_operation_details")
 		length = len(ho_details)
-		balance = self.total_tonnage_mt - ho_details[length-1].period_tonnage_running_total
-		self.balance = balance
+		if ho_details[length-1].period_tonnage_running_total:
+			balance = self.total_tonnage_mt - ho_details[length-1].period_tonnage_running_total
+			self.balance = balance

16ad8f6a72ff6e92fe9b9d740be5faec5955b26d
durve-greycube <zelam@greycube.in>
2024-06-07 10:38:56 +0530

    fix: add filter for item in stock entry, item json clean up

---
+++ kict/public/js/stock_entry.js
@@ -0,0 +1,15 @@
+frappe.ui.form.on("Stock Entry", {
+    onload: function(frm){
+        frm.set_query("item_code","items", function (doc,cdt,cdn){
+            if (frm.doc.stock_entry_type=="Handling Loss" || frm.doc.stock_entry_type=="Audit Shortage" || frm.doc.stock_entry_type=="Cargo Received") {
+                return {
+                    filters: {
+                        is_stock_item: 1,
+                        is_customer_provided_item: 1
+                    },
+                };       
+            }
+
+        })
+    }
+})

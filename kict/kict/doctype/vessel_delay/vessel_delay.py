5084b400019b0344560999eb09456ca98daa0cfb
durve-greycube <zelam@greycube.in>
2024-06-11 15:22:14 +0530

    fix: validate delay accounts in vessel delay

--- kict/kict/doctype/vessel_delay/vessel_delay.py
+++ kict/kict/doctype/vessel_delay/vessel_delay.py
@@ -9,6 +9,7 @@ class VesselDelay(Document):
 	def validate(self):
 			self.set_total_hours()
 			self.set_total_delay_hours()
+			self.validate_terminal_account()
 		
 	def set_total_hours(self):  
 		for row in self.get("vessel_delay_details"):
@@ -24,3 +25,12 @@ class VesselDelay(Document):
 				total_delay_hours = total_delay_hours + row.total_hours
 			print(total_delay_hours,"  total_delay_hours")
 			self.vessel_total_delay_hours = total_delay_hours				
+
+	def validate_terminal_account(self):
+		terminal_account_found = False
+		terminal_account = frappe.db.get_single_value("Coal Settings","vessel_delay_account_for_terminal")
+		for row in self.vessel_delay_details:
+			if row.account_delays == terminal_account and terminal_account_found==False:
+				terminal_account_found=True
+			elif row.account_delays == terminal_account and terminal_account_found==True:
+				frappe.throw(_("Row #{0}: You cannot select {1} account again.").format(row.idx,row.account_delays))			

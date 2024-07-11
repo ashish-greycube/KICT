32278b57a24bd73a518fa273f4b59e0636400edc
durve-greycube <zelam@greycube.in>
2024-07-11 13:06:12 +0530

    fix:set query for meal, validation

--- kict/kict/doctype/employee_fooding/employee_fooding.js
+++ kict/kict/doctype/employee_fooding/employee_fooding.js
@@ -1,8 +1,28 @@
 // Copyright (c) 2024, GreyCube Technologies and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Employee Fooding", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Employee Fooding", {
+    setup(frm){
+        frappe.db.get_single_value('Coal Settings', 'food_meal_item_group').then(group => {
+            frm.set_query("meal_type", function () {
+                return {
+                    filters: {
+                        item_group: group
+                    }
+                }
+            })
+        })
+    },
+	meal_type(frm) {
+        frappe.call({
+            method: "kict.kict.doctype.employee_fooding.employee_fooding.get_meal_price",
+            args: {
+                "meal_type": frm.doc.meal_type,
+            },
+            callback: function (response) {
+                console.log(response.message)
+                frm.set_value("meal_price",response.message)
+            }
+        })
+	},
+});
--- kict/kict/doctype/employee_fooding/employee_fooding.py
+++ kict/kict/doctype/employee_fooding/employee_fooding.py
@@ -1,9 +1,36 @@
 # Copyright (c) 2024, GreyCube Technologies and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+from frappe import _
 from frappe.model.document import Document
 
-
 class EmployeeFooding(Document):
-	pass
+	def validate(self):
+		self.validate_meal_price()
+		self.calculate_borne_by_company_and_employee()
+
+	def validate_meal_price(self):
+		if self.meal_price == None:
+			frappe.throw(_("Please set meal price"))
+
+	def calculate_borne_by_company_and_employee(self):
+		if self.meal_price:
+			meal_price = self.meal_price
+			percentage_borne_by_company = frappe.db.get_single_value("Coal Settings","food_borne_by_company")
+			borne_by_company = meal_price * (percentage_borne_by_company / 100)
+			borne_by_employee = meal_price - borne_by_company
+			self.borne_by_company = borne_by_company
+			self.borne_by_employee = borne_by_employee
+
+@frappe.whitelist()
+def get_meal_price(meal_type):
+	meal_price_list = frappe.db.get_single_value("Coal Settings","food_meal_price_list")
+	from erpnext.stock.get_item_details import get_price_list_rate_for
+	args=frappe._dict({
+		'price_list':meal_price_list,
+		'qty':1,
+		'uom':frappe.db.get_value('Item', meal_type, 'stock_uom')})
+	return get_price_list_rate_for(args,meal_type)		
+
+

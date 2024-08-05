1c6444182ff7256ac77dfcfaf4274fa4043bc7de
Sakshi-Greycube <sakshi@greycube.in>
2024-08-05 11:37:52 +0530

    feat: validate employee fooding - Employee + Date + Item

--- kict/kict/doctype/employee_fooding/employee_fooding.py
+++ kict/kict/doctype/employee_fooding/employee_fooding.py
@@ -4,11 +4,13 @@
 import frappe
 from frappe import _
 from frappe.model.document import Document
+from frappe.utils import get_link_to_form
 
 class EmployeeFooding(Document):
 	def validate(self):
 		self.validate_meal_price()
 		self.calculate_borne_by_company_and_employee()
+		self.validate_unique_employee_fooding()
 
 	def validate_meal_price(self):
 		if self.meal_price == None:
@@ -27,6 +29,15 @@ class EmployeeFooding(Document):
 			elif is_meal_subsidised == 0:
 				self.borne_by_employee = meal_price
 				self.borne_by_company = 0
+
+	def validate_unique_employee_fooding(self):
+		prev_list = frappe.db.get_all('Employee Fooding', 
+								filters={'employee': self.employee, 'meal_date': self.meal_date, 'meal_type': self.meal_type, 'name': ['!=', self.name]},
+								fields=['name'])
+		
+		if len(prev_list) > 0:
+			frappe.throw(_('This Employee Fooding is already exist - {0}.'
+				  .format(get_link_to_form('Employee Fooding', prev_list[0].name))))
 
 @frappe.whitelist()
 def get_meal_price(meal_type):

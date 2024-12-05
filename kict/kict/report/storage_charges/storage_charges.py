fe869f5b9308db04f94ac2b3aab69dd2bf3be333
ashish-greycube <mr.ashish.shah@gmail.com>
2024-12-05 15:18:34 +0530

    fix: group by show_date

--- kict/kict/report/storage_charges/storage_charges.py
+++ kict/kict/report/storage_charges/storage_charges.py
@@ -372,9 +372,7 @@ def get_stock_ledger_entries_for_batch_bundle(filters):
 				item.customer,
 				sle.posting_time,
 				case 
-					when (sle.posting_time > '06:00:00'
-					and sle.posting_time <= '23:59:59')
-						then  sle.posting_date
+					when (sle.posting_time > '06:00:00'	and sle.posting_time <= '23:59:59')	then  sle.posting_date
 					else date_add(sle.posting_date, INTERVAL -1 DAY)  
 				end as show_date,				
 				sum(batch_package.qty) as actual_qty,

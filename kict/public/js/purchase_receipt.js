d7c9fdb6f631a3c766f91263d80a445a41ce016c
durve-greycube <zelam@greycube.in>
2026-03-11 13:10:51 +0530

    fix:condition for inspection point mandatory in purchase receipt

--- kict/public/js/purchase_receipt.js
+++ kict/public/js/purchase_receipt.js
@@ -1,15 +1,17 @@
 frappe.ui.form.on("Purchase Receipt", {
     onload(frm) {
-        let logged_in_user_roles = frappe.user_roles
-        console.log(logged_in_user_roles)
-        if (logged_in_user_roles.length > 0){
-            logged_in_user_roles.forEach(element => {
-                if (element.includes("Verifier")) {
-                    frm.set_df_property("custom_packing_condition", "reqd", 1);
-                    frm.set_df_property("custom_surface_finish", "reqd", 1);
-                    frm.set_df_property("custom_physical_damage", "reqd", 1);
-                }
-            });
-        } 
+        if (frm.doc.custom_receipt_type == "Goods Receipt") {
+            let logged_in_user_roles = frappe.user_roles
+            console.log(logged_in_user_roles)
+            if (logged_in_user_roles.length > 0){
+                logged_in_user_roles.forEach(element => {
+                    if (element.includes("Verifier")) {
+                        frm.set_df_property("custom_packing_condition", "reqd", 1);
+                        frm.set_df_property("custom_surface_finish", "reqd", 1);
+                        frm.set_df_property("custom_physical_damage", "reqd", 1);
+                    }
+                });
+            } 
+        }
     }
 })
